2c607c1d6a578336a2847aade15ef6dab91b6d4c
Valentin Demeusy <valentin.demeusy@imavie.fr>
2016-02-25 20:30:36 -0500

    add : fully working three.js demo

--- utils/three/app.js
+++ utils/three/app.js
@@ -1,34 +1,18 @@
-var app = angular.module('myApp', [ 'ngSanitize', 'adaptv.adaptStrap' ]);
-app.controller('myCtrl', function($scope, $http) {
-    jQuery.ajax({
-        dataType: "json",
-        url: "../tsvToJson/atlasStructure.json",
-        async: false,
-        success: function (data) {
-        var atlasStructure = data;
-        var vtkDatasources = data.filter(function (object) { 
-        return object['@type']==='datasource' && /\.vtk$/.test(object.source);
-    });
-    var vtkDatasourcesId = vtkDatasources.map(source => source["@id"]);
-    var vtkStructures = [];
-    for(var i=0; i<atlasStructure.length; i++) {
-        var item = atlasStructure[i];
-        if (item['@type']==='structure') {
-            var dataSourceIndex = vtkDatasourcesId.indexOf(item.sourceSelector.dataSource);
-            if ( dataSourceIndex> -1) {
-                //item refers to a vtk file
-                item.sourceSelector.dataSourceObject = vtkDatasources[dataSourceIndex];
-                vtkStructures.push(item);
-            }
-        }
-    }
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+var container, stats;
+
+var camera, controls, scene, renderer;
 
 
-    //THREE.JS INIT
-    var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 1e10 );
+init();
+
+function init() {
+
+    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 1e10 );
     camera.position.z = 300;
 
-    var controls = new THREE.TrackballControls( camera );
+    controls = new THREE.TrackballControls( camera );
 
     controls.rotateSpeed = 5.0;
     controls.zoomSpeed = 5;
@@ -40,7 +24,7 @@ app.controller('myCtrl', function($scope, $http) {
     controls.staticMoving = true;
     controls.dynamicDampingFactor = 0.3;
 
-    var scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
     scene.add( camera );
 
@@ -52,155 +36,185 @@ app.controller('myCtrl', function($scope, $http) {
     camera.add( dirLight );
     camera.add( dirLight.target );
 
+    jQuery.ajax({
+        dataType: "json",
+        url: "../tsvToJson/atlasStructure.json",
+        async: true,
+        success: dealWithAtlasStructure
+    });
 
-
-    //Load all the vtk files
-    var loader = new THREE.VTKLoader();
-    var loadedFile = 0;
-    var numberOfFilesToLoad = vtkStructures.length;
-
-    //this function enables us to create a scope and then keep the right item in the callback
-    function loadVTKFile (i) {
-        var file = vtkStructures[i].sourceSelector.dataSourceObject.source;
-        loader.load( file, function ( geometry ) {
-
-            var item = vtkStructures[i];
-
-            geometry.computeVertexNormals();
-
-            var rgb = item.renderOptions.color.match(/^rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,?\s*([.0-9]+)?\)$/);
-            if (rgb) {
-                rgb = rgb.map(Number);
+    function dealWithAtlasStructure (data) {
+        var atlasStructure = data;
+        var vtkDatasources = data.filter(function (object) { 
+            return object['@type']==='datasource' && /\.vtk$/.test(object.source);
+        });
+        var vtkDatasourcesId = vtkDatasources.map(source => source["@id"]);
+        var vtkStructures = [];
+        for(var i=0; i<atlasStructure.length; i++) {
+            var item = atlasStructure[i];
+            if (item['@type']==='structure') {
+                var dataSourceIndex = vtkDatasourcesId.indexOf(item.sourceSelector.dataSource);
+                if ( dataSourceIndex> -1) {
+                    //item refers to a vtk file
+                    item.sourceSelector.dataSourceObject = vtkDatasources[dataSourceIndex];
+                    vtkStructures.push(item);
+                }
             }
-            else {
-                console.log(JSON.stringify(item,null,4));
-                rgb = [0,0,0,0];
-            }
+        }
 
-            var material = new THREE.MeshLambertMaterial({
-                wireframe : false, 
-                morphTargets : false, 
-                side : THREE.DoubleSide, 
-                color : rgb[1]*256*256+rgb[2]*256+rgb[3]
+
+        //Load all the vtk files
+        var loader = new THREE.VTKLoader();
+        var loadedFile = 0;
+        var numberOfFilesToLoad = vtkStructures.length;
+
+        //send the modal a signal to give the number of vtk files to load
+        angular.element(document.body).scope().$root.$broadcast('modal.JSONLoaded', numberOfFilesToLoad);
+
+        //this function enables us to create a scope and then keep the right item in the callback
+        function loadVTKFile (i) {
+            var file = vtkStructures[i].sourceSelector.dataSourceObject.source;
+            loader.load( file, function ( geometry ) {
+
+                var item = vtkStructures[i];
+
+                geometry.computeVertexNormals();
+
+                var rgb = item.renderOptions.color.match(/^rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,?\s*([.0-9]+)?\)$/);
+                if (rgb) {
+                    rgb = rgb.map(Number);
+                }
+                else {
+                    console.log(JSON.stringify(item,null,4));
+                    rgb = [0,0,0,0];
+                }
+
+                var material = new THREE.MeshLambertMaterial({
+                    wireframe : false, 
+                    morphTargets : false, 
+                    side : THREE.DoubleSide, 
+                    color : rgb[1]*256*256+rgb[2]*256+rgb[3]
+                });
+
+                console.log(rgb[1]*256*256+rgb[2]*256+rgb[3]);
+
+
+                material.opacity = item.renderOptions.opacity || rgb[4] || 1.0;
+                material.visible = true;
+
+
+                if (material.opacity < 1) {
+                    material.transparent = true;
+                }
+
+
+                var mesh = new THREE.Mesh( geometry, material );
+                item.mesh = mesh;
+                loadedFile++;
+
+                //signal to the modal
+                angular.element(document.body).scope().$root.$broadcast('modal.fileLoaded');
+
+                if (loadedFile === numberOfFilesToLoad) {
+                    //put it in an immediate timeout to give the browser the opportunity to refresh the modal
+                    setTimeout(createHierarchy,0);
+                }
+
             });
-
-            material.opacity = item.renderOptions.opacity || rgb[4] || 1.0;
-            material.visible = true;
-
-            if (material.opacity < 1) {
-                material.transparent = true;
-            }
-
-            var mesh = new THREE.Mesh( geometry, material );
-            item.mesh = mesh;
-
-            loadedFile++;
-
-            if (loadedFile === numberOfFilesToLoad) {
-                createHierarchy();
-            }
-
-        } );
-    }
-
-
-    for (var i = 0; i<vtkStructures.length; i++) {
-        loadVTKFile(i);
-    }
-
-
-    function getTreeObjectFromUuid (uuid) {
-        var item = atlasStructure.find(x=>x['@id']===uuid);
-        var treeObject = {
-            name : item.annotation.name,
-            mesh : item.mesh
-        };
-        if (item['@type']==='group') {
-            treeObject.children = item.members.map(getTreeObjectFromUuid).filter(x => x.mesh !== undefined);
-            treeObject.mesh = new THREE.Group();
-            for (var i = 0; i< treeObject.children.length; i++) {
-                try {
-                    treeObject.mesh.add(treeObject.children[i].mesh);
-                }
-                catch (e) {
-                    console.log(e);
-                }
-            }
-        }
-        return treeObject;
-    }
-
-    function createHierarchy () {
-        var rootGroups = atlasStructure.filter(x => x['@type']==='group' && x.annotation && x.annotation.root);
-        var hierarchyTree = {
-            children : rootGroups.map(group => getTreeObjectFromUuid(group['@id']))
-        };
-
-        for(var i = 0; i<hierarchyTree.children.length; i++) {
-            scene.add(hierarchyTree.children[i].mesh);
         }
 
 
-        $scope.data = {
-            root : hierarchyTree
-        };
+        for (var i = 0; i<vtkStructures.length; i++) {
+            loadVTKFile(i);
+        }
 
-        var listContainer = document.getElementById('structureList');
-        listContainer.innerHTML = `<ad-tree-browser class="ad-border-default"
-tree-name="treeDemoBordered"
-row-ng-class="{added:item._selected}"
-tree-root="data.root"
-child-node="children"
-children-padding="15"
-bordered="true"
-node-template-url="treeNode.html">
-</ad-tree-browser>`;
+        function getTreeObjectFromUuid (uuid) {
+            var item = atlasStructure.find(x=>x['@id']===uuid);
+            var treeObject = {
+                name : item.annotation.name,
+                mesh : item.mesh
+            };
+            if (item['@type']==='group') {
+                treeObject.children = item.members.map(getTreeObjectFromUuid).filter(x => x.mesh !== undefined);
+                treeObject.mesh = new THREE.Group();
+                for (var i = 0; i< treeObject.children.length; i++) {
+                    try {
+                        treeObject.mesh.add(treeObject.children[i].mesh);
+                    }
+                    catch (e) {
+                        console.log(e);
+                    }
+                }
+            }
+            return treeObject;
+        }
 
-        console.log('end controller');
+        function createHierarchy () {
+            var rootGroups = atlasStructure.filter(x => x['@type']==='group' && x.annotation && x.annotation.root);
+            var hierarchyTree = {
+                children : rootGroups.map(group => getTreeObjectFromUuid(group['@id']))
+            };
+
+            for(var i = 0; i<hierarchyTree.children.length; i++) {
+                scene.add(hierarchyTree.children[i].mesh);
+            }
+
+
+            var listContainer = document.getElementById('structureList');
+            var treeDirective = document.getElementById('treeListDirective');
+
+            angular.element(document.body).scope().$root.$broadcast('insertTree',hierarchyTree);
+
+            //send a signal to the modal
+            angular.element(document.body).scope().$root.$broadcast('modal.hierarchyLoaded', vtkStructures.length);
+
+
+            console.log('end controller');
+        }
+
+        // renderer
+
+        container = document.getElementById('rendererFrame');
+        camera.aspect = container.clientWidth / container.clientHeight;
+
+        renderer = new THREE.WebGLRenderer( { antialias: false } );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( container.clientWidth, container.clientHeight );
+
+        container.appendChild( renderer.domElement );
+
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.top = '0px';
+        container.appendChild( stats.domElement );
+
+        //
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
+        animate();
+
     }
+}
 
+function onWindowResize() {
 
-    // renderer
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
 
-    container = document.getElementById( 'rendererFrame' );
-    renderer = new THREE.WebGLRenderer( { antialias: false } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( container.width, container.height );
+    renderer.setSize( container.clientWidth, container.clientHeight );
 
-    container.appendChild( renderer.domElement );
-
-    stats = new Stats();
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.top = '0px';
-    container.appendChild( stats.domElement );
-
-    function onWindowResize() {
-
-        camera.aspect = container.width / container.height;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize( container.width, container.height );
-
-        controls.handleResize();
-
-    }
-
-    window.addEventListener( 'resize', onWindowResize, false );
-
-    function animate() {
-
-        requestAnimationFrame( animate );
-
-        controls.update();
-        renderer.render( scene, camera );
-
-        stats.update();
-
-    }
-
-    animate();
-
+    controls.handleResize();
 
 }
-               });
-});
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    controls.update();
+    renderer.render( scene, camera );
+
+    stats.update();
+
+}
---
+++ utils/three/modalController.js
@@ -0,0 +1,62 @@
+angular.module('atlasDemo').controller('ModalDemoCtrl', function ($scope, $uibModal, $log) {
+
+    var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'modalContent.html',
+        controller: 'ModalInstanceCtrl',
+        resolve: {}
+    });
+
+    /*
+    modalInstance.result.then(function (selectedItem) {
+        $scope.selected = selectedItem;
+    }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+    });
+    */
+
+
+});
+
+// Please note that $uibModalInstance represents a modal window (instance) dependency.
+// It is not the same as the $uibModal service used above.
+
+angular.module('atlasDemo').controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, $rootScope) {
+
+    $scope.loadingJSON = true;
+    $scope.loadingVTK = false;
+    $scope.loadingHierarchy = false;
+    
+    $scope.loadedVTKFiles = 0;
+    $scope.numberOfVTKFiles = 1;
+    
+    $scope.done = false;
+    
+    $rootScope.$on('modal.JSONLoaded', function (event, numberOfVTKFiles) {
+        $scope.loadingJSON = false;
+        $scope.loadingVTK = true;
+        $scope.numberOfVTKFiles = numberOfVTKFiles;
+    });
+    
+    $rootScope.$on('modal.fileLoaded', function () {
+        $scope.loadedVTKFiles++;
+        if ($scope.loadedVTKFiles === $scope.numberOfVTKFiles) {
+            $scope.loadingVTK = false;
+            $scope.loadingHierarchy = true;
+        }
+    });
+    
+    $rootScope.$on('modal.hierarchyLoaded', function () {
+        $scope.loadingHierarchy = false;
+        $scope.done = true;
+    });
+    
+    $scope.ok = function () {
+        $uibModalInstance.close(true);
+    };
+    /*
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+    */
+});
