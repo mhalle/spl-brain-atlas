8de8aa05f593f4b248542555e1bee075a0bca715
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-03-09 01:13:06 -0500

    enh : update submodule

--- utils/configMrmlToJson.js
+++ utils/configMrmlToJson.js
@@ -3,5 +3,17 @@ module.exports = {
     mrmlFileLocation : "../../slicer/brain-atlas.mrml",
     colorTableFileLocation : "../../slicer/colortables/hncma-atlas-lut.ctbl",
     vtkFilesDirectory : "../../slicer/models/",
-    jsonResultFileName : "atlasStructure.json"
+    jsonResultFileName : "atlasStructure.json",
+    header : {
+        "@type": "header",
+        "species": "human",
+        "organ": "brain",
+        "name" : "The SPL/NAC Brain Atlas",
+        "license" : "?",
+        "citation" : "?",
+        "version" : "1",
+        "contact" : "https://github.com/stity/spl-brain-atlas",
+        "comment" : "",
+        "coordinate_system" : "self defined"
+    }
 }
