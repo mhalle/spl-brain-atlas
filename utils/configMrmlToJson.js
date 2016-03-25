1d523d1649bc678b11d33d7f4506bbac0254fbe2
Valentin Demeusy <valentin.demeusy@gmail.com>
2016-03-24 23:02:01 -0400

    enh : update and run new mrmlToJson script
    Signed-off-by: Valentin Demeusy <valentin.demeusy@gmail.com>

--- utils/configMrmlToJson.js
+++ utils/configMrmlToJson.js
@@ -1,6 +1,6 @@
 //all the path given relatively to mrmlToJson.js script
 module.exports = {
-    mrmlFileLocation : "../../slicer/brain-atlas.mrml",
+   mrmlFileLocation : "../../slicer/brain-atlas.mrml",
     colorTableFileLocation : "../../slicer/colortables/hncma-atlas-lut.ctbl",
     vtkFilesDirectory : "../../slicer/models/",
     jsonResultFileName : "../atlasStructure.json",
@@ -15,7 +15,15 @@ module.exports = {
         "contact" : "https://github.com/stity/spl-brain-atlas",
         "comment" : "",
         "coordinate_system" : "self defined",
-        "roots" : [],
-        "backgroundImages" : "../../slicer/volumes/imaging/A1_grayT1.nrrd"
-    }
-}
+        "roots" : []
+    },
+    labelMapFiles : [{ 
+        name : "../../slicer/volumes/labels/hncma-atlas.nrrd",
+        includes : "*" 
+    },{ 
+        name : "../../slicer/volumes/labels/skin.nrrd",
+        includes : [3]
+    }],
+    "backgroundImages" : ["../../slicer/volumes/imaging/A1_grayT1.nrrd", "../../slicer/volumes/imaging/A1_grayT2.nrrd"]
+
+};
